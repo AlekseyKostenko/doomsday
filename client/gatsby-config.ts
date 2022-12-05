44d69c8345c40215db8b1cb21fb56846b13e9e24
Alexey <kostenko.20101994@mail.ru>
2022-12-05 18:40:07 +0300

    Redirect from http to https

--- client/gatsby-config.ts
+++ client/gatsby-config.ts
@@ -20,7 +20,15 @@ module.exports = {
       options: {
         RewriteBase: true,
         https: true,
-        www: true,                
+        www: true,
+        host: 'часы-судного-дня.рф',
+        redirect: [
+          'RewriteRule ^not-existing-url/?$ /existing-url [R=301,L,NE]',
+          {
+            from: 'http://xn-----6kcnec4ddfb1ab0a5c8c1d.xn--p1ai',
+            to: 'https://xn-----6kcnec4ddfb1ab0a5c8c1d.xn--p1ai',
+          },
+        ],
       },
     },
     {
